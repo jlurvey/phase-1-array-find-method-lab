6ce0d8c7c86db0580cf7bf13f3648c0ec16fa2f5
Joseph Lurvey <joeylurvey@gmail.com>
2023-05-09 19:32:52 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(array) {
+    const win = array.find(function(game) {
+        return game.result === "W";
+    });
+
+    if (win) {
+        return win.year;
+    }
+};
